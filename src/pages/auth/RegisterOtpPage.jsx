09c2fb78ba8670cf04565a0896774df12a28b619
Aditi <aditiupadhyay839@gmail.com>
2026-05-18 19:30:50 +0530

    fix:placeholder added in register page

--- src/pages/auth/RegisterOtpPage.jsx
+++ src/pages/auth/RegisterOtpPage.jsx
@@ -78,7 +78,6 @@ export default function RegisterOtpPage() {
               registration={register("firstName")}
               error={errors.firstName}
               autoComplete="given-name"
-              placeholder="John"
             />
             <FormField
               id="lastName"
